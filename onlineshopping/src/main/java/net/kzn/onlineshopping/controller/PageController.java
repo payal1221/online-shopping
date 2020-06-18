83a8dc5825459f43e04dd3b710c90b15d12afb8c
payal1221 <payalgupta1221@gmail.com>
2020-06-18 18:03:23 +0530

    completed the interface theme and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -13,27 +13,39 @@ public class PageController {
 	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to spring Web MVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
 	
-	@RequestMapping(value ="/test")
-	public ModelAndView test(@RequestParam(value="greeting" ,required=false) String greeting) {
-		if(greeting==null) {
-			greeting = "Hello there";
-		}
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","About us");
+		mv.addObject("userClickAbout",true);
 		return mv;
-		
 	}
 	
-	@RequestMapping(value ="/test1/{greeting}")
-	public ModelAndView test1(@PathVariable("greeting") String greeting) {
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
-		return mv;
-		
-	}
 
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact us");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
+	/*
+	 * @RequestMapping(value ="/test") public ModelAndView
+	 * test(@RequestParam(value="greeting" ,required=false) String greeting) {
+	 * if(greeting==null) { greeting = "Hello there"; } ModelAndView mv = new
+	 * ModelAndView("page"); mv.addObject("greeting",greeting); return mv;
+	 * 
+	 * }
+	 * 
+	 * @RequestMapping(value ="/test1/{greeting}") public ModelAndView
+	 * test1(@PathVariable("greeting") String greeting) { ModelAndView mv = new
+	 * ModelAndView("page"); mv.addObject("greeting",greeting); return mv;
+	 * 
+	 * }
+	 */
 }
